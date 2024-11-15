f5fc59faacdf13d9006f3d10a599f7610c506614
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-15 17:28:32 +0100

    Update app.py+geocode eurazia

--- app.py
+++ app.py
@@ -11,19 +11,28 @@ def get_coordinates(location_name, country_name):
     else:
         return None, None
 
-# Functie om een lijst van landen te verkrijgen (voor Eurazië)
-def get_all_countries():
-    countries = []
-    for country in pycountry.countries:
-        countries.append((country.name, country.alpha_2))  # (Landnaam, Landcode)
-    return countries
+# Functie om landen in Eurazië op te halen (Europa, Azië en Nabije Oosten)
+def get_eurasian_countries():
+    # Lijst van landen in Eurazië (gebaseerd op de geografische regio)
+    eurasian_countries = [
+        "Albania", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina",
+        "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "Georgia",
+        "Germany", "Greece", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Israel",
+        "Italy", "Kazakhstan", "Kosovo", "Kuwait", "Kyrgyzstan", "Latvia", "Liechtenstein", "Lithuania",
+        "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Morocco", "Nepal", "Netherlands", "Norway",
+        "Oman", "Pakistan", "Palestinian Territories", "Poland", "Portugal", "Qatar", "Romania", "Russia",
+        "San Marino", "Saudi Arabia", "Serbia", "Singapore", "Slovakia", "Slovenia", "South Korea", "Spain",
+        "Sri Lanka", "Syria", "Tajikistan", "Thailand", "Turkey", "Turkmenistan", "Ukraine", "United Kingdom",
+        "Uzbekistan", "Vietnam", "Yemen"
+    ]
+    return eurasian_countries
 
 # Streamlit app
 def main():
     st.title("Plaatsselectie met Landkeuze (Eurazië)")
 
-    # Haal alle landen op
-    countries = get_all_countries()
+    # Haal de lijst van Euraziatische landen op
+    countries = get_eurasian_countries()
 
     # Plaatsinvoer en landenkeuze
     col1, col2 = st.columns([3, 1])
@@ -32,7 +41,7 @@ def main():
         location_name = st.text_input("Voer de naam van de plaats in:")
     
     with col2:
-        country_name = st.selectbox("Kies een land:", [country[0] for country in countries], index=0)
+        country_name = st.selectbox("Kies een land:", countries, index=0)
     
     # Zoek naar de coördinaten van de opgegeven locatie en land
     if st.button("Zoeken"):
