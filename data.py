82c99410d89f46ea825667258dc98bdb69e5ef4f
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-16 22:16:54 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Functie voor de nieuwe expander (Under Construction)
+def show_data_expander():
+    with st.expander("Under Construction", expanded=False):  # Deze expander is niet uitgeklapt
+        st.write("Under Construction")  # Plaats hier de tekst die je wilt tonen
