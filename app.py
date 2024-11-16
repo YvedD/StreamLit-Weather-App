56ad5e2557b131e1e9cc8cd095294252442d2467
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-16 22:41:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from invoer import show_input_form
 from data import show_data_expander  # Gebruik de hernoemde functie uit data.py
 
 # Titel van de project
-st.markdown('<div class="project-title">Migration Weather Data</div>', unsafe_allow_html=True)
+#st.markdown('<div class="project-title">Migration Weather Data</div>', unsafe_allow_html=True)
 
 # Toon de invoer
 show_input_form()  # Roep de invoer functie aan uit invoer.py
