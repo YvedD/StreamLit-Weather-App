c3c79e98bb9bdfdfca37eca8d5471dacc7b34592
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-16 22:17:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-import invoer  # Dit is de import van je invoer.py module
+from invoer import show_input_form
+from data import show_data_expander  # Gebruik de hernoemde functie uit data.py
 
-# Roep de invoerfunctie aan
-invoer.show_input_form()
+# Titel van de project
+st.markdown('<div class="project-title">Migration Weather Data</div>', unsafe_allow_html=True)
+
+# Toon de invoer
+show_input_form()  # Roep de invoer functie aan uit invoer.py
+
+# Toon de nieuwe expander (Under Construction)
+show_data_expander()  # Roep de functie voor de expander uit data.py
