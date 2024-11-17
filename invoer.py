15d4ad4cf12991a020592da8510279c22f4c649b
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-17 11:26:09 +0100

    Update invoer.py

--- invoer.py
+++ invoer.py
@@ -81,7 +81,7 @@ def show_input_form():
 
     # Voeg enkel de titel toe boven de expander
     st.markdown(
-        '<h1 style="font-size: 36px; font-weight: bold; color: #4CAF50; margin-bottom: 20px;">Migration Weather Data</h1>',
+        '<h1 style="font-size: 36px; font-weight: bold; color: #4CAF50; margin-bottom: 20px;">Migration Historic Weather Data - 3 day Forecast</h1>',
         unsafe_allow_html=True
     )
 
