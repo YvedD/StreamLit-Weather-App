23ce179d996489db6d41767b7ad717ff4522383d
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-17 11:30:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from data import show_data_expander  # Gebruik de hernoemde functie uit data.py
 
 # Titel van de project
 #st.markdown('<div class="project-title">Migration Weather Data</div>', unsafe_allow_html=True)
+# Toon de nieuwe expander (Under Construction)
+show_data_expander()  # Roep de functie voor de expander uit data.py
 
 # Toon de invoer
 show_input_form()  # Roep de invoer functie aan uit invoer.py
 
-# Toon de nieuwe expander (Under Construction)
-show_data_expander()  # Roep de functie voor de expander uit data.py
