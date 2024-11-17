8fd54cd1877dc0866f69cb9154811f3b154d6b13
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-17 18:52:53 +0100

    Update data.py

--- data.py
+++ data.py
@@ -9,6 +9,8 @@ def show_data_expander():
     country = st.session_state.get('country', 'Unknown')  # Haal het land op, met 'Unknown' als default
     location = st.session_state.get('location', 'Unknown')  # Haal de locatie op
     selected_date = st.session_state.get('selected_date', 'Unknown')  # Haal de datum op, met 'Unknown' als default
+    start_hour = st.session_state.get('start_hour', 'Unknown')  # Haal het startuur op
+    end_hour = st.session_state.get('end_hour', 'Unknown')  # Haal het einduur op
     latitude = st.session_state.get('latitude', 'Unknown')  # Haal de latitude op
     longitude = st.session_state.get('longitude', 'Unknown')  # Haal de longitude op
     sunrise = st.session_state.get('sunrise', 'Unknown')  # Haal de sunrise op
@@ -22,6 +24,8 @@ def show_data_expander():
         st.write(f"**Longitude:** {longitude}")
         st.write(f"**Country:** {country}")
         st.write(f"**Date:** {selected_date}")
+        st.write(f"**Start Hour:** {start_hour}")
+        st.write(f"**End Hour:** {end_hour}")
 
         if sunrise != 'Unknown' and sunset != 'Unknown':
             st.write(f"**Sunrise:** {sunrise}")
