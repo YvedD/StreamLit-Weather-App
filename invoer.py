07628fdafcd84bac578858cb1622da360ae6110a
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-18 22:29:17 +0100

    Update invoer.py

--- invoer.py
+++ invoer.py
@@ -208,8 +208,7 @@ def show_input_form():
         st.write(f"**{country}**, **{location}**, **GPS** :{latitude:.2f}°N {longitude:.2f}°E")
         st.write(f"**{start_hour_label}**: {start_hour_input.strftime('%H:%M')}, **{end_hour_label}**: {end_hour_input.strftime('%H:%M')} ")
         # Toon overige tijden voor controle
-        st.write(f"**{sunrise_label}**: {sunrise}, **{sunset_label}**: {sunset}")
-        st.write(f"**{civil_twilight_label} Begin**: {civil_twilight_begin}, **{civil_twilight_label} End**: {civil_twilight_end}")
+        st.write(f"**{sunrise_label}**: {sunrise}, **{sunset_label}**: {sunset}, **{civil_twilight_label} Begin**: {civil_twilight_begin}, **{civil_twilight_label} End**: {civil_twilight_end}")
         st.write(f"**{nautical_twilight_label} Begin**: {nautical_twilight_begin}, **{nautical_twilight_label} End**: {nautical_twilight_end}")
 
         return latitude, longitude, location
