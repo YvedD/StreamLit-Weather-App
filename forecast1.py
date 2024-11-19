391b387b350c5042ad8154f4ac8cfcb4531f4d94
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-19 14:52:50 +0100

    Update forecast1.py

--- forecast1.py
+++ forecast1.py
@@ -88,7 +88,7 @@ def show_forecast1_expander():
             name="Temperatuurkaart",
             overlay=True,
             control=True,
-            opacity=2.0  # Verhoogde opaciteit voor helderdere kleuren
+            opacity=0.4  # Verhoogde opaciteit voor helderdere kleuren
         ).add_to(m)
 
         # Weergeef de kaart binnen Streamlit met st_folium
