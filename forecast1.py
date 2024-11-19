ad5e022f5bb6b73b2b0ff6e3fec45dd989fec466
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-19 18:04:12 +0100

    Update forecast1.py

--- forecast1.py
+++ forecast1.py
@@ -4,9 +4,14 @@ from streamlit_folium import st_folium
 from datetime import datetime, timedelta
 import os
 
-# Haal de API-sleutel op uit de omgeving
-api_key = os.getenv("API_KEY")  # Leest de API-sleutel vanuit de omgeving
+# Haal de API-sleutel op uit de omgevingsvariabelen
+api_key = os.getenv("API_KEY_OPENWEATHERMAPS")
 
+# Controleer of de API-sleutel is opgehaald
+if api_key:
+    st.write("API Key is geladen.")  # Dit is optioneel om te testen
+else:
+    st.write("API Key is niet gevonden!")  # Dit helpt bij troubleshooting
 def show_forecast1_expander():
     # Bepaal de datum van "vandaag + 1 dag"
     forecast_date = datetime.now() + timedelta(days=1)
