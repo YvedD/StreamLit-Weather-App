6e60aa8fae47f693d5a7a5bf75b4125058ae3c1a
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-20 19:23:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from invoer import show_input_form
 from maps import show_map_expander
 from data import show_data_expander  # Importeer de data-expander
+from forecast2 import show_forecast2_expander
 from forecast1 import show_forecast1_expander
 
 def main():
