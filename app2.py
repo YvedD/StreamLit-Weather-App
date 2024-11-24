effd36aaef39a98e9fd8f7bf42fd7152d03f4fe0
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-24 16:23:37 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,10 +2,19 @@ import streamlit as st
 from invoer import show_input_form
 from maps import show_map_expander
 from data import show_data_expander  # Importeer de data-expander
+from forecast2 import show_forecast2_expander
+from forecast1 import show_forecast1_expander
+from forecastchart import show_weather_chart_expander
+
+st.set_page_config(layout="wide", page_title="Migration Weather app")
 
 def main():
     # Verkrijg invoer van de gebruiker
     latitude, longitude, location = show_input_form()
+
+    # Altijd de data-expander tonen (zelfstandig van de locatie)
+    show_data_expander()  # Toon de data-expander voor de opgegeven locatie
+
 
     # Toon de kaart op basis van de invoer
     if latitude and longitude:
@@ -15,7 +24,13 @@ def main():
         st.error("Invalid location coordinates.")  # Foutmelding als de locatie niet geldig is
 
     # Altijd de data-expander tonen (zelfstandig van de locatie)
-    show_data_expander()  # Toon de data-expander voor de opgegeven locatie
+    #show_data_expander()  # Toon de data-expander voor de opgegeven locatie
 
+    #toon een weerkaart (test)!
+    show_forecast1_expander()
+    #toon de voorspellingen voor -1dag+7 dagen
+    show_forecast2_expander()
+    #toon de Echart
+    #show_weather_chart_expander()
 if __name__ == "__main__":
     main()
