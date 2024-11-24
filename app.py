d3d1aade67aa811f988ac3c3e6fd994d95e0bad7
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-24 19:27:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def main():
     # Voorbeeld start- en eindtijden op basis van de civiele zonsopgang en zonsondergang (zoals hierboven opgehaald)
     civil_sunrise_local = time(6, 30)  # Starttijd van de civiele zonsopgang (voorbeeld)
     civil_sunset_local = time(20, 45)  # Eindtijd van de civiele zonsondergang (voorbeeld)
+    
+    normal_sunrise_local = time(7, 00)  # Starttijd van de normale zonsopgang (voorbeeld)
+    normal_sunset_local = time(19, 30)  # Eindtijd van de normale zonsondergang (voorbeeld)
+    
+    nautical_sunrise_local = time(5, 45)  # Starttijd van de nautische zonsopgang (voorbeeld)
+    nautical_sunset_local = time(21, 00)  # Eindtijd van de nautische zonsondergang (voorbeeld)
 
     # Sidebar configuratie
     with st.sidebar:
@@ -38,8 +44,28 @@ def main():
         latitude = st.number_input("Latitude", value=51.2389)
         longitude = st.number_input("Longitude", value=2.9724)
 
+        # Keuze voor de zonsopgang/ondergang
+        zonsopgang_keuze = st.radio(
+            "Selecteer type zonsopgang/zonsondergang",
+            options=["Normal", "Civil", "Nautical"],
+            index=1,  # Standaard op 'Civil'
+        )
+
+        # Sla de geselecteerde optie op in de session_state
+        if "zonsopgang_keuze" not in st.session_state:
+            st.session_state.zonsopgang_keuze = "Civil"
+        st.session_state.zonsopgang_keuze = zonsopgang_keuze
+        
+        # Kies de juiste zonsopgang/zonsondergang tijden op basis van de keuze
+        if zonsopgang_keuze == "Normal":
+            start_hour, end_hour = normal_sunrise_local, normal_sunset_local
+        elif zonsopgang_keuze == "Nautical":
+            start_hour, end_hour = nautical_sunrise_local, nautical_sunset_local
+        else:  # 'Civil' is de default
+            start_hour, end_hour = civil_sunrise_local, civil_sunset_local
+
         # Voeg de tijdsinterval slider toe met de civiele zonsopgang en zonsondergang als standaard
-        appointment = create_time_slider(civil_sunrise_local, civil_sunset_local)
+        appointment = create_time_slider(start_hour, end_hour)
 
     # Maak tabs aan voor de verschillende secties
     tab1, tab2, tab3 = st.tabs(["Weatherdata", "Temperature Forecast", "Multiday Forecast"])
