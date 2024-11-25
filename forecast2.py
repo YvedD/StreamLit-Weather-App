10ab3a7812cac653c3127e02df2257865f911d50
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-25 13:17:06 +0100

    Update forecast2.py

--- forecast2.py
+++ forecast2.py
@@ -118,7 +118,7 @@ def show_forecast2_expander():
             st.error("Kan de weergegevens niet ophalen. Controleer de API URL.")
             return None
 
-    st.title(f"**Weersvoorspelling voor {location}** (-1d/+5d)")
+    st.subheader(f"**Weersvoorspelling voor {location}** (-1d/+5d)")
 
     weather_data = fetch_weather_data(API_URL)
 
