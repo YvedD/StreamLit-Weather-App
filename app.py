8b3027f9eba26b17fd15bd5677aa52bf175e4091
Yves De Saedeleer <ydsdsy@gmail.com>
2024-11-25 14:02:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
     #toon een weerkaart (test)!
     show_forecast1_expander()
     #toon de voorspellingen voor -1dag+7 dagen
-    show_forecast2_expander()
+    #show_forecast2_expander()
     #toon de Echart
     #show_weather_chart_expander()
 if __name__ == "__main__":
